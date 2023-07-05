e47a292528c3bdacf0a55bffc2b1c4677f216722
Dragon <christianlonghi@outlook.it>
2023-07-05 19:52:18 +0200

    Added iterators constructor

--- src/map.h
+++ src/map.h
@@ -14,12 +14,13 @@ public:
 	Map() noexcept;
 	Map(std::initializer_list<Entity> ents);
 
+	template< class It >
+	Map(It begin, It end);
+
 	size_t size() const noexcept;
 
 	Entity* begin() noexcept;
 	Entity* end() noexcept;
-
-	bool collide(Entity target);
 
 	Map(Map&&) noexcept = default;
 	Map& operator=(Map&&) noexcept = default;
@@ -29,4 +30,13 @@ private:
 	std::unique_ptr<Entity[]> _arr;
 };
 
-bool collision(Entity block, Entity target);
+template <class It>
+inline Map::Map(It begin, It end) 
+	: _len{ static_cast<size_t>(end - begin) } {
+	_arr = std::make_unique<Entity[]>(_len);
+
+	size_t i = 0;
+	for (auto it = begin; it != end; it++) {
+		_arr[i++] = std::move(*it);
+	}
+}
